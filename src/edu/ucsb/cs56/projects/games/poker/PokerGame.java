977da21ac83dfbcbac06325901691755d5466873
Spencer Prescott <stp@umail.ucsb.edu>
2015-03-04 02:00:34 -0800

    Keep track of wins and add fold button

--- src/edu/ucsb/cs56/projects/games/poker/PokerGame.java
+++ src/edu/ucsb/cs56/projects/games/poker/PokerGame.java
@@ -17,14 +17,17 @@ public class PokerGame {
 	private JPanel panel;
 	private JFrame mainFrame,mainFrame2;
 	private JFrame playButtonFrame;
-	private JButton playButton, playAgainButton, turnButton, riverButton;
-	private JLabel winnerLabel;
+	private JButton playButton, playAgainButton, turnButton, riverButton, foldButton;
+	private JLabel winnerLabel, playerWinsLabel, dealerWinsLabel;
 	private JPanel dealerPanel, playerPanel, centerPanel, revealPanel;
 	private Hand dealerHand, playerHand, flop;
 	private Deck deck;
 	private Card turnCard, riverCard, backCard;
 	private ImageIcon backCardImage;
-	
+  private static int playerWins = 0;
+  private static int dealerWins = 0;
+  private static int playerWon = 2;
+
 /**
 	No arg constructor that initializes a new deck.
 */
@@ -48,9 +51,10 @@ public class PokerGame {
 		
 		playButtonFrame = new JFrame();
 		playButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	
-		
+
+		playerWins = 0;
+    dealerWins = 0;
+
 		panel=new JPanel();
 		
 		playButton=new JButton("Click to Play");
@@ -106,6 +110,23 @@ public class PokerGame {
 		
 	}
 
+/**
+  Method for when user folds
+*/
+
+  public void fold()
+  {
+    dealerWins++;
+    if (playerWon == 0) {
+      playerWins--;
+    }
+    // Reset player win flag
+    playerWon = 2;
+    mainFrame.dispose();
+    PokerGame gui2=new PokerGame();
+    gui2.replay();
+  }
+
 
 /**
   Method lays out GUI elements
@@ -114,12 +135,16 @@ public class PokerGame {
   public void layoutSubViews()
   {
       playerSetUp();
-      
+
       dealerPanel=new JPanel();
       playerPanel=new JPanel();
       centerPanel=new JPanel();
       revealPanel=new JPanel();
+      dealerWinsLabel = new JLabel();
+      playerWinsLabel = new JLabel();
       turnButton=new JButton("TURN");
+      foldButton = new JButton("FOLD");
+      foldButton.addActionListener(new foldButtonListener());
       turnButton.addActionListener(new turnButtonListener());
       for(int i=0;i<2;i++){
         dealerPanel.add(new JLabel(backCardImage));
@@ -133,15 +158,29 @@ public class PokerGame {
         
       dealerPanel.add(new JLabel("DEALER"));
       playerPanel.add(new JLabel("PLAYER"));
+      playerPanel.add(foldButton);
       centerPanel.add(BorderLayout.SOUTH,turnButton);
-      if(playerHand.compareHands(dealerHand)==1){
+
+      playerWinsLabel.setText(String.format("Player wins: %d", playerWins));
+      dealerWinsLabel.setText(String.format("Dealer wins: %d", dealerWins));
+      playerPanel.add(playerWinsLabel);
+      dealerPanel.add(dealerWinsLabel);
+
+
+      if(playerHand.compareHands(dealerHand) == 1) {
+        playerWins = playerWins + 1;
+        playerWon = 0;
         winnerLabel=new JLabel("YOU WON!");
-      }
-      else if(playerHand.compareHands(dealerHand)==2){
+        System.out.println("I win");
+      } else if(playerHand.compareHands(dealerHand) == 2) {
+        playerWon = 1;
         winnerLabel=new JLabel("TIED");
-      }
-      else{
+        System.out.println("No one wins");
+      } else {
+        playerWon = 1;
+        dealerWins = dealerWins + 1;
         winnerLabel=new JLabel("Dealer won");
+        System.out.println("You win");
       }
       /**dealerPanel.add(new JLabel(Integer.toString(dealerHand.getHandValue())));
       playerPanel.add(new JLabel(Integer.toString(playerHand.getHandValue())));
@@ -151,9 +190,11 @@ public class PokerGame {
         for(int i:playerHand.sortHand()){
           playerPanel.add(new JLabel(Integer.toString(i)));
         }*/
+
+
+
       deck.reShuffle();
         
-      
       mainFrame=new JFrame();
       mainFrame.setSize(600,600);
       mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -211,8 +252,9 @@ public class PokerGame {
   		centerPanel.add(winnerLabel);
   		centerPanel.add(playAgainButton);
   		
-  		for(int i=0;i<2;i++)
+  		for(int i=0;i<2;i++) {
   			revealPanel.add(new JLabel(getCardImage(dealerHand.get(i))));
+      }
   		revealPanel.add(new JLabel("DEALER"));
   		
   		mainFrame.remove(dealerPanel);
@@ -221,7 +263,15 @@ public class PokerGame {
   		mainFrame.setVisible(true);
   	}
   }
-  
+
+/**
+  Fold button actions
+*/
+  class foldButtonListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+      fold();
+    }
+  }
 /**
 	Sets up the Poker game when the Client clicks the PlayAgain button.
 */
