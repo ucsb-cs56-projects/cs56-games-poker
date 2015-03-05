ce56e6e9bcb5175aac08d93d3a25a7d0a5057224
Spencer Prescott <stp@umail.ucsb.edu>
2015-03-04 21:36:22 -0800

    Update javadoc

--- src/edu/ucsb/cs56/projects/games/poker/Player.java
+++ src/edu/ucsb/cs56/projects/games/poker/Player.java
@@ -17,44 +17,84 @@ public class Player {
         return pokerHand;
     }
 
+    /**
+     * Sets the player's poker hand
+     * @param pokerHand the hand the player will have
+     */
     public void setPokerHand(Hand pokerHand) {
         this.pokerHand = pokerHand;
     }
 
+    /**
+     * One arg constructor
+     * @param chips amount of chips player starts with
+     */
     public Player(int chips) {
         this.chips = chips;
         this.wins = 0;
     }
 
+    /**
+     * No arg constructor
+     */
     public Player() {
         this.chips = 0;
         this.wins = 0;
     }
 
+    /**
+     * Get player's poker hand
+     * @return Hand object
+     */
     public Hand getHand() {
         return pokerHand;
     }
 
+    /**
+     * Set player's poker hand
+     * @param hand
+     */
     public void setHand(Hand hand) {
         this.pokerHand = hand;
     }
 
+    /**
+     * Get number of player wins
+     * @return int type
+     */
     public int getWins() {
         return wins;
     }
 
+    /**
+     * Set player wins
+     * @param wins number of wins to give to player
+     */
     public void setWins(int wins) {
         this.wins = wins;
     }
 
+    /**
+     * Get number of chips player has
+     * @return int type
+     */
     public int getChips() {
         return chips;
     }
 
+    /**
+     * Set number of chips player has
+     * @param chips number of chips to give to player
+     */
     public void setChips(int chips) {
         this.chips = chips;
     }
 
+    /**
+     * Bet chips
+     * @param _chips number of chips to bet
+     * @return int type: number of chips bet
+     */
     public int bet(int _chips) {
         if (chips == 0) return 0;
         if (chips <= _chips) {
@@ -67,18 +107,33 @@ public class Player {
         }
     }
 
+    /**
+     * Add Card obejct to poker hand
+     * @param card card to add to hand
+     */
     public void addCardToHand(Card card) {
         pokerHand.add(card);
     }
 
+    /**
+     * Get card from Hand
+     * @param index index of card into Hand ArrayList
+     * @return Card object of the card at the specified index
+     */
     public Card getCardFromHand(int index) {
         return pokerHand.get(index);
     }
 
+    /**
+     * Increment player's wins by one
+     */
     public void win() {
         wins += 1;
     }
 
+    /**
+     * Fold hand, calls delegate fold method
+     */
     public void foldHand() {
         delegate.fold();
     }
--- src/edu/ucsb/cs56/projects/games/poker/PlayerDelegate.java
+++ src/edu/ucsb/cs56/projects/games/poker/PlayerDelegate.java
@@ -1,5 +1,11 @@
 package edu.ucsb.cs56.projects.games.poker;
 
+/**
+ * Delegate for player folding
+ */
 public interface PlayerDelegate {
+    /**
+     * Fold delegate method, define in controller to deal with chip winnings
+     */
     public void fold();
 }
