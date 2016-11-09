be5ad76ccf28dca1e1a0d3e0dfc7f60081ddbb2b
Greg Whiter <Greg Whiter>
2016-11-09 11:57:30 -0800

    Attempt at issue #37

--- src/edu/ucsb/cs56/projects/games/poker/PokerGameGui.java
+++ src/edu/ucsb/cs56/projects/games/poker/PokerGameGui.java
@@ -74,8 +74,8 @@ public class PokerGameGui extends PokerGame{
 		rulesPart2 = new JPanel();
 		rulesPart3 = new JPanel();
 		rulesPart1.setLayout(new BoxLayout(rulesPart1, BoxLayout.Y_AXIS));
-		rulesPart2.setLayout(new BoxLayout(rulesPart1, BoxLayout.Y_AXIS));
-		rulesPart3.setLayout(new BoxLayout(rulesPart1, BoxLayout.Y_AXIS));
+		rulesPart2.setLayout(new BoxLayout(rulesPart2, BoxLayout.Y_AXIS));
+		rulesPart3.setLayout(new BoxLayout(rulesPart2, BoxLayout.Y_AXIS));
 	    rulesOverviewImg = new JLabel();
 		rulesGameplayImg = new JLabel();
 		rulesExampleImg = new JLabel();
