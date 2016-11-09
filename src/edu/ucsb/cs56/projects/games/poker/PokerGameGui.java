4efc2730627660e84eea56cb5868dd04ead82cee
gwhiter <g_whiter@umail.ucsb.edu>
2016-11-09 13:00:10 -0800

    Rules Button WIP

--- src/edu/ucsb/cs56/projects/games/poker/PokerGameGui.java
+++ src/edu/ucsb/cs56/projects/games/poker/PokerGameGui.java
@@ -389,14 +389,7 @@ public class PokerGameGui extends PokerGame{
     protected class rulesButtonHandler implements ActionListener {// rules
 	public void actionPerformed(ActionEvent e) {
 	    //  mainFrame.setSize(1000, 1000);	
-	   	//rulesPanel.setVisible(!rulesPanel.isVisible() );
-	   	if(!rulesPanel.isVisible()){
-			rulesPanel.setVisible(true);
-			rulesPart1.setVisible(true);
-			rulesPanel = rulesPart1;
-		}
-		else
-			rulesPanel.setVisible(false);
+	   	rulesPanel.setVisible(!rulesPanel.isVisible() );
 	}
     }
 
