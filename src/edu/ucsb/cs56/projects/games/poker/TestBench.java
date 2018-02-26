c664a6075455dbda38633de4a8633bd60120d8d9
Dan Hemgren <dhemgren@kth.se>
2018-02-26 11:45:51 +0100

    added AnalyzeCoverage-function (WIP #9)

--- src/edu/ucsb/cs56/projects/games/poker/TestBench.java
+++ src/edu/ucsb/cs56/projects/games/poker/TestBench.java
@@ -1,5 +1,13 @@
 package edu.ucsb.cs56.projects.games.poker;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+/**
+ * This class holds all data structures and functions
+ * relating to our DIY branch coverage tracker
+ * @author danhemgren
+ *
+ */
 
 public class TestBench {
 	public static HashMap<String, Boolean> coverage;
@@ -17,5 +25,24 @@ public class TestBench {
 		coverage.put("compareHands9", false);
 		coverage.put("compareHands10", false);
 		coverage.put("compareHands11", false);
+	}
+	
+	public static void AnalyzeCoverage() {
+		Collection<Boolean> values = coverage.values();
+		int totalNumberOfBranches = values.size();
+		if(totalNumberOfBranches < 1) {
+			System.out.println("No branches have been registered, aborting...");
+			return;
+		}
+		int traversedBranches = 0;
+		Iterator it = values.iterator();
+		while(it.hasNext()) {
+			Boolean val = (Boolean) it.next();
+			if(val == true) {
+				traversedBranches++;
+			}
+		}
+		double res = ((double)traversedBranches / (double)totalNumberOfBranches)*100;
+		System.out.println("Total branch coverage is " + res + "%");
 	}
 }
