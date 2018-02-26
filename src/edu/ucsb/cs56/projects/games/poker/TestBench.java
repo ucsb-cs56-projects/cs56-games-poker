b6c327e298fbf30fb5284f17c32288b98109869a
Dan Hemgren <dhemgren@kth.se>
2018-02-26 11:38:28 +0100

    added SetupCoverageTracking, test bench setup method (WIP #9)

--- src/edu/ucsb/cs56/projects/games/poker/TestBench.java
+++ src/edu/ucsb/cs56/projects/games/poker/TestBench.java
@@ -3,4 +3,19 @@ import java.util.HashMap;
 
 public class TestBench {
 	public static HashMap<String, Boolean> coverage;
+	
+	public static void SetupCoverageTracking() {
+		coverage = new HashMap<String, Boolean>();
+		coverage.put("compareHands1", false);
+		coverage.put("compareHands2", false);
+		coverage.put("compareHands3", false);
+		coverage.put("compareHands4", false);
+		coverage.put("compareHands5", false);
+		coverage.put("compareHands6", false);
+		coverage.put("compareHands7", false);
+		coverage.put("compareHands8", false);
+		coverage.put("compareHands9", false);
+		coverage.put("compareHands10", false);
+		coverage.put("compareHands11", false);
+	}
 }
